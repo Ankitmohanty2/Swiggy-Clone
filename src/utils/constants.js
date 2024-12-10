55a88c669d70b3b62896ff685dd2b5ccf2cd566c
Ankit Mohanty <117025304+Ankitmohanty2@users.noreply.github.com>
2024-12-10 10:49:50 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,9 +2,11 @@ export const FETCH_CARDS_URL = "https://corsproxy.io/?https://www.swiggy.com/dap
 
 export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
+const CORS_URL ="https://cors-handlers.vercel.app/";
+
 export const LOGO_URL = "https://corsproxy.io/?https://images-platform.99static.com/A_Ax0GQuo_NHI0Y7XZHmFtGfBDY=/0x0:1000x1000/500x500/top/smart/99designs-contests-attachments/126/126252/attachment_126252018"
 
-export const MENU_API = "https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId="
+export const MENU_API = "${CORS_URL}api/?url=https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Fmenu%2Fpl%3Fpage-type%3DREGULAR_MENU%26complete-menu%3Dtrue%26catalog_qa%3Dundefined%26submitAction%3DENTER%26restaurantId%3D"
 
 export const PURE_VEG_IMAGE = "https://corsproxy.io/?https://media.istockphoto.com/id/1297289529/vector/leaf-icon-vector-stock-illustration.jpg?s=612x612&w=0&k=20&c=dFtiy2BLo20FR5OeJg932L1BW4UNXhC-f-zew5vfLz0=";
 
@@ -12,7 +14,7 @@ export const AUTOCOMPLETE_URL = "https://corsproxy.io/?https://www.swiggy.com/da
 
 export const ADDRESS_RECOMMEND_URL = 'https://corsproxy.io/?https://www.swiggy.com/dapi/misc/address-recommend?place_id=';
 
-export const UPDATE_RESTAURANTS_LIST_URL = 'https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING';
+export const UPDATE_RESTAURANTS_LIST_URL = '${CORS_URL}api/?url=https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Frestaurants%2Flist%2Fv5%3Fis-seo-homepage-enabled%3Dtrue%26page_type%3DDESKTOP_WEB_LISTING%26';
 
 
 // Rightly Arranged
